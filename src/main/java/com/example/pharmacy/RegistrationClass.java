1db31a057a905346d4237c5218924dad430e0ff1
Mox <tenmaken246@gmail.com>
2025-02-23 23:43:44 +0300

    переходы по кнопкам в другие окна

--- src/main/java/com/example/pharmacy/RegistrationClass.java
+++ src/main/java/com/example/pharmacy/RegistrationClass.java
@@ -1,11 +1,16 @@
 package com.example.pharmacy;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class RegistrationClass {
 
@@ -29,7 +34,37 @@ public class RegistrationClass {
 
     @FXML
     void initialize() {
+        registration.setOnAction(event -> {
 
+            String firstName = First_name.getText();
+            String log = login.getText();
+            String pass = password.getText();
+
+            User user = new User(firstName, log, pass);
+            JDBCTable.writesql(user);
+
+            if(!firstName.equals("") && !log.equals("") && !pass.equals("")){
+                openNewWin("/com/example/pharmacy/Main.fxml");
+            }
+        });
+
+    }
+    public void openNewWin(String path){
+        registration.getScene().getWindow().hide();
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource(path));
+
+        try {
+            loader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Parent root = loader.getRoot();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.showAndWait();
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.pharmacy {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.pharmacy to javafx.fxml;
