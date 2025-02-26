83cf339b7f3d326be9290494089433ece3ae0554
Mox <tenmaken246@gmail.com>
2025-02-26 23:50:43 +0300

    новая табл

--- src/main/java/com/example/pharmacy/TablDB.java
+++ src/main/java/com/example/pharmacy/TablDB.java
@@ -7,4 +7,12 @@ public class TablDB {
     public static final String LOGIN_USERS = "login";
     public static final String PASS_USERS = "pass";
 
+    public static final String NAME_TABLE_PILLS = "pills";
+
+    public static final String ID_USER_PILLS = "iduser";
+    public static final String NAME_PILLS = "namepills";
+    public static final String DESCRIPTION_PILLS = "description";
+    public static final String QUANTITY_PILLS = "quantity";
+    public static final String DATA_PILLS = "expiratiodate";
+
 }
