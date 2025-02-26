ba284ff9feec8cbba32255df66922a84f08fb4de
Mox <tenmaken246@gmail.com>
2025-02-26 23:51:27 +0300

    объединила данные с табл

--- src/main/java/com/example/pharmacy/MainClass.java
+++ src/main/java/com/example/pharmacy/MainClass.java
@@ -1,12 +1,21 @@
 package com.example.pharmacy;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.Date;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MainClass {
 
@@ -18,6 +27,39 @@ public class MainClass {
 
     @FXML
     private Menu All_pharm;
+
+    ObservableList<Pills> dataPills = FXCollections.observableArrayList(read());
+
+    public ArrayList<Pills> read(){
+        ResultSet rs = JDBCTable.readPillsSQL();
+        ArrayList<Pills> arrPills = new ArrayList<>();
+
+        try {
+            if(rs.next()){
+                String name = rs.getString(TablDB.NAME_PILLS);
+                String description = rs.getString(TablDB.DESCRIPTION_PILLS);
+                int quantity = rs.getInt(TablDB.QUANTITY_PILLS);
+                Date expiratiodate = rs.getDate(TablDB.DATA_PILLS);
+
+                arrPills.add(new Pills(name, description, quantity, expiratiodate));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return arrPills;
+    }
+
+    @FXML
+    private TableColumn<Pills, Date> DAta;
+
+    @FXML
+    private TableColumn<Pills, String> Description;
+
+    @FXML
+    private TableColumn<Pills, String> Name;
+
+    @FXML
+    private TableColumn<Pills, Integer> Quantity;
 
     @FXML
     private Menu actions;
@@ -41,11 +83,16 @@ public class MainClass {
     private MenuItem sort_Z_A;
 
     @FXML
-    private TableView<?> table_data;
+    private TableView<Pills> table_data;
 
     @FXML
     void initialize() {
+        Name.setCellValueFactory(new PropertyValueFactory<>("name"));
+        Description.setCellValueFactory(new PropertyValueFactory<>("description"));
+        Quantity.setCellValueFactory(new PropertyValueFactory<>("quantity"));
+        DAta.setCellValueFactory(new PropertyValueFactory<>("expiratioD"));
 
+        table_data.setItems(dataPills);
     }
 
 }
