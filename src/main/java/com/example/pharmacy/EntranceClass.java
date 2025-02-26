23e2c36b6bb859829b060fef7f3d861d4be9a91a
Mox <tenmaken246@gmail.com>
2025-02-26 23:51:02 +0300

    пеерход на новое окно

--- src/main/java/com/example/pharmacy/EntranceClass.java
+++ src/main/java/com/example/pharmacy/EntranceClass.java
@@ -2,6 +2,8 @@ package com.example.pharmacy;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -40,7 +42,7 @@ public class EntranceClass {
             String pass = password.getText();
 
             if(!log.equals("") && !pass.equals("")){
-                // написать вход в сиситему (авторизация)
+                setLogin(log, pass);
             }
         });
 
@@ -65,6 +67,21 @@ public class EntranceClass {
         Stage stage = new Stage();
         stage.setScene(new Scene(root));
         stage.showAndWait();
+    }
+
+    public void setLogin(String log, String pass){
+        User user = User.getUser();
+        user.setLogin(log);
+        user.setPassword(pass);
+        ResultSet rs = JDBCTable.readUserSQL(user);
+
+        try {
+            if(rs.next()){
+                openNewWin("/com/example/pharmacy/Main.fxml");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
