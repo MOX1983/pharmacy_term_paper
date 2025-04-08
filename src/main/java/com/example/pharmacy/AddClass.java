d02fc609843b9d2e60db931fa267721c3693ed7b
Mox <tenmaken246@gmail.com>
2025-04-08 17:57:30 +0300

    изменила добавление данных в таблицу (картинки )

--- src/main/java/com/example/pharmacy/AddClass.java
+++ src/main/java/com/example/pharmacy/AddClass.java
@@ -1,6 +1,9 @@
 package com.example.pharmacy;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,6 +13,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.scene.image.Image;
 
 public class AddClass {
 
@@ -35,7 +41,30 @@ public class AddClass {
     private Button addButten;
 
     @FXML
+    private Button add_img;
+
+    @FXML
+    private ImageView imgFile;
+
+    private byte[] imageBytes;
+
+    @FXML
     void initialize() {
+
+        add_img.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Выберите изображение");
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Изображения", "*.png", "*.jpg", "*.jpeg"));
+            File file = fileChooser.showOpenDialog(add_img.getScene().getWindow());
+
+            if(file != null){
+                Image image = new Image(file.toURI().toString());
+                imgFile.setImage(image);
+
+                imageBytes = convertImageToBytes(file);
+            }
+        });
 
         addButten.setOnAction(event -> {
             User user = User.getUser();
@@ -45,7 +74,7 @@ public class AddClass {
             int qua = Integer.parseInt(quantity.getText());
             Date d = Date.valueOf(date.getValue());
 
-            Pills pill = new Pills(name, description, qua, d);
+            Pills pill = new Pills(name, description, qua, d, imageBytes);
 
             ResultSet rs = JDBCTable.readUserSQL(user);
             try {
@@ -58,6 +87,15 @@ public class AddClass {
             JDBCTable.insertPills(pill);
         });
 
+    }
+
+    private byte[] convertImageToBytes(File file) {
+        try {
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
 }
